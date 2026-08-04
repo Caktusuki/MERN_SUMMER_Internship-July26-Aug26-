b46b163e092d533a964b054a0cdf8d3b92959216
Caktus_uki <cactusuki@gmail.com>
2026-08-04 13:11:23 +0530

    feat: add Card component and integrate into App

--- Lec16/adtu/src/App.jsx
+++ Lec16/adtu/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Card from './Card.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,9 @@ function App() {
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <Card name="card1" price="1000" />
+      <Card name="card2" price="2000" />
+      <Card name="card3" price="3000" />
     </>
   )
 }
